049bb7b1123fc084d3fda0c363740be660a2fc2c
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-10-20 00:43:28 -1000

    Add pseudocode of game program
    Very basic, will most likely change

---
+++ script.js
@@ -0,0 +1,8 @@
+/**PB
+ * factory to create players
+ * something to start the game
+ * something to control the flow of the game
+ * something to determine when there is a winner or a draw/no winner
+ * something to keep track of wins
+ * something to restart the board
+ */
