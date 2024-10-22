97c6264d860c1daab58d7caece26a94b09452953
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-10-22 01:39:14 -1000

    Build factory that creates players
    Factory creates players but the logic of the module is off, need to fix

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // task 1: build a GameBoard and display it when "start" is clicked
+// task 2: grab players info and attach it to a mark "X" or "O"
 const GameBoard = (() => {
     let gameBoard = ["", "", "", "", "", "", "", "", ""]
     const gameContainer = document.querySelector(".game-container");
@@ -15,26 +16,42 @@ const GameBoard = (() => {
     return { renderBoard }
 })();
 
-function createPlayers(name, symbol) {
-    return {
-        name: name,
-        symbol: symbol,
-    }
-}
+
 
 const GamePlayers = (() => {
-
-})
-
-playerOneInput = document.getElementById("player-one-name");
-playerTwoInput = document.getElementById("player-two-name");
-function returnPlayerValue(player) {
-    if (player.value === "") {
-        console.log(player.placeholder)
-    } else {
-        console.log(player.value);
+    function createPlayers(input, symbol) {
+        if (input.value === "" ) {
+            return {
+                name: input.placeholder,
+                symbol: symbol,
+            }
+        }
+        else if (input.value !== "") {
+            return {
+                name: input.value,
+                symbol: symbol,
+            }
+        }
     }
-}
+
+    // what am i even trying to do rn
+    // create players one and two in this function and hold them as objects
+    // return players one and two but ONLY when the start button is clicked
+    // so the logic is click the start button, and then the two players are created
+
+    function playersOneTwo() {
+        // stuff
+    }
+
+    // maybe put all this in a function
+    playerOneInput = document.getElementById("player-one-name");
+    playerTwoInput = document.getElementById("player-two-name");
+
+    const playerOne = () => createPlayers(playerOneInput, "X");
+    const playerTwo = () => createPlayers(playerTwoInput, "O");
+
+    return { createPlayers }
+})();
 
 
 const startBtn = document.getElementById("start");
