4c8d328acd54310a60e1f7218a6f030f25f28e54
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-10-25 00:34:04 -1000

    Delete second js file
    Changed my mind,
    I'll figure it out

--- script2.js
+++
@@ -1,6 +0,0 @@
-// JS file that doesn't utilize modules and stuff
-// Mentally struggling to wrap head around the concept
-// This file is going to create the function of the game but with global variables
-// The idea is to convert this file into a file with modules and stuff
-// But I'm taking too long figuring out the mental process of modules
-// So I'm hoping converting the file will help
