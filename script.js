32cc6c4729abfcda44f9350083bae3f1aea6cf94
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-10-24 15:37:52 -1000

    Add module that handles game display messages

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // task 1: build a GameBoard and display it when "start" is clicked
 // task 2: grab players info and attach it to a mark "X" or "O"
+
+const GameDisplay = (() => {
+    const displayMessage = (message) => {
+        document.querySelector(".game-message").innerHTML = message;
+    }
+
+    return { displayMessage }
+})();
+
 const GameBoard = (() => {
     let gameBoard = ["", "", "", "", "", "", "", "", ""]
     const gameContainer = document.querySelector(".game-container");
@@ -17,50 +26,30 @@ const GameBoard = (() => {
 })();
 
 
-
-const GamePlayers = (() => {
-    function createPlayers(input, symbol) {
-        if (input.value === "" ) {
-            return {
-                name: input.placeholder,
-                symbol: symbol,
-            }
-        }
-        else if (input.value !== "") {
-            return {
-                name: input.value,
-                symbol: symbol,
-            }
+function createPlayers(input, symbol) {
+    if (input.value === "" ) {
+        return {
+            name: input.placeholder,
+            symbol: symbol,
         }
     }
-
-    // what am i even trying to do rn
-    // create players one and two in this function and hold them as objects
-    // return players one and two but ONLY when the start button is clicked
-    // so the logic is click the start button, and then the two players are created
-
-    function playersOneTwo() {
-        // stuff
+    else if (input.value !== "") {
+        return {
+            name: input.value,
+            symbol: symbol,
+        }
     }
-
-    // maybe put all this in a function
-    playerOneInput = document.getElementById("player-one-name");
-    playerTwoInput = document.getElementById("player-two-name");
-
-    const playerOne = () => createPlayers(playerOneInput, "X");
-    const playerTwo = () => createPlayers(playerTwoInput, "O");
-
-    return { createPlayers }
-})();
+};
 
 
 const startBtn = document.getElementById("start");
 startBtn.addEventListener("click", () => {
     GameBoard.renderBoard();
-})
+    GameController.start();
+});
 
 
 const restartBtn = document.getElementById("restart");
 restartBtn.addEventListener("click", () => {
     console.log("Baskemtball");
-})
+});
